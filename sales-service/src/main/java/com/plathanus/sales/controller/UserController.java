c98e0ad0055f867d4e7b8927b4000d826042bcb5
Diego Roberto <diego-roberto@hotmail.com>
2025-04-24 20:51:31 -0300

    Paginação no endpoint de users

--- sales-service/src/main/java/com/plathanus/sales/controller/UserController.java
+++ sales-service/src/main/java/com/plathanus/sales/controller/UserController.java
@@ -3,11 +3,14 @@ package com.plathanus.sales.controller;
 import com.plathanus.sales.dto.UserDTO;
 import com.plathanus.sales.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -17,8 +20,12 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public List<UserDTO> listAll() {
-        return userService.findAll();
+    public ResponseEntity<Page<UserDTO>> listAll(
+                                                @RequestParam(defaultValue = "0") int page,
+                                                @RequestParam(defaultValue = "10") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<UserDTO> pageResult = userService.findAll(pageable);
+        return ResponseEntity.ok(pageResult);
     }
 
 }
--- sales-service/src/main/java/com/plathanus/sales/service/UserService.java
+++ sales-service/src/main/java/com/plathanus/sales/service/UserService.java
@@ -4,6 +4,9 @@ import com.plathanus.sales.config.AuthServiceProperties;
 import com.plathanus.sales.dto.UserDTO;
 import com.plathanus.sales.util.TokenUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -11,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -20,7 +24,7 @@ public class UserService {
     private final RestTemplate restTemplate;
     private final AuthServiceProperties properties;
 
-    public List<UserDTO> findAll() {
+    public Page<UserDTO> findAll(Pageable pageable) {
         String url = properties.getUrl() + "/users";
 
         HttpHeaders headers = new HttpHeaders();
@@ -34,7 +38,26 @@ public class UserService {
                 entity,
                 UserDTO[].class
         );
-        return List.of(response.getBody());
+
+        UserDTO[] userDTOArray = response.getBody();
+        if (userDTOArray == null || userDTOArray.length == 0) {
+            return Page.empty(pageable);
+        }
+        List<UserDTO> userDTOList = Arrays.asList(userDTOArray);
+
+        int pageSize = pageable.getPageSize();
+        int currentPage = pageable.getPageNumber();
+        int start = currentPage * pageSize;
+        int end = Math.min(start + pageSize, userDTOList.size());
+
+        List<UserDTO> currentPageList = userDTOList.subList(start, end);
+
+        return new PageImpl<>(
+                currentPageList,
+                pageable,
+                userDTOList.size()
+        );
+
     }
 
 }
